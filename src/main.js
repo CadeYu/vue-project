eb52afc84eefd4d7baaeafaf6be402dcda065ff6
CadeYu <yu83612457@gmail.com>
2024-05-04 23:58:31 +0800

    api test

--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
 import './assets/main.css'
-
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { testAPI } from './apis/testAPI'
+
+
 
 const app = createApp(App)
 
+testAPI().then(res => {
+    console.log(res)
+})
+
+
 app.use(createPinia())
 app.use(router)
-
+app.use(ElementPlus)
 app.mount('#app')
